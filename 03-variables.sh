41ec55d013e6d225f28479e615898f4e6aa52f2b
kbalakrishna <koyadabalu@gmail.com>
2024-09-12 22:58:35 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=Raheem
+person2=Nasar
+
+echo "$person1 :: Hi $person2, How are you?"
+echo "$person2 :: Hi $person1, I am good, How is going?"
+echo "$person1 :: I am doing good, whatz going on in oyur life?"
+echo "$person2 :: I started learning shell script person1."
