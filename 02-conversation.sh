420f060727db46b52697ee0bd4253f9de8cb60d1
kbalakrishna <koyadabalu@gmail.com>
2024-09-12 22:21:52 +0530

    I created a new conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh :: Hi Suresh, How are you?"
+echo "Suresh :: Hi Ramesh, I am good, How is going?"
+echo "Ramesh :: I am doing good, whatz going on in oyur life?"
+echo "Suresh :: I started learning shell script Ramesh."
