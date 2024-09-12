79a59ce55539521b70c53ce7cf1c0db627ade62e
kbalakrishna <koyadabalu@gmail.com>
2024-09-12 23:06:51 +0530

    added 05-variables

---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+echo "please enter your name::"
+read USERNAME 
+echo -s "username entered is:$USERNAME"
+echo "please enter your password::"
+read -s PASSWORD
+echo "password entered is ::$PASSWORD"
