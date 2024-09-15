332575ba34fcb684db53cdbbbb4412ff9c097b4f
kbalakrishna <koyadabalu@gmail.com>
2024-09-15 17:39:33 +0530

    functions

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "All the variables passed to the script : $@ "
+echo "Number of variables passed : $# "
+echo "script name : $0 "
+echo "current working directory : $PWD "
+echo "Home directory of current user : $HOME "
+echo "PID of the script executing now : $$ "
+
