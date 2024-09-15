31993c5facfe422ce3b1e00443117d07b766add4
kbalakrishna <koyadabalu@gmail.com>
2024-09-15 16:54:59 +0530

    I changed in array

--- 06-array.sh
+++ 06-array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Fruits=("Appple" "kiwi" "orange")
-echo "First fruit is : $Fruits[0]"
-echo "second fruit is : $Fruits[1]"
-echo "Third fruit is : $Fruits[2]"
-echo "Fruits are : $Fruits[@]"
+echo "First fruit is : ${Fruits[0]}"
+echo "second fruit is : ${Fruits [1]}"
+echo "Third fruit is : ${Fruits [2]}"
+echo "Fruits are : ${Fruits[@]}"
