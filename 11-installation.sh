0ed26318ba358c18997abef76ab1ca6c811984a0
kbalakrishna <koyadabalu@gmail.com>
2024-09-19 21:48:36 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-USERID= $(id -u)
+USERID=$(id -u)
 
-if [$USERID -ne 0]
-  then 
-  echo "please run this script with root privileges"
-  exit 1
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root privileges"
+    exit 1
 fi
 
 dnf list installed git
-if [$? -ne 0]
+if [ $? -ne 0 ]
   then
-  echo "git is not installed , going to install it"
-
-  dnf install git -y
-  if [ $? -ne 0 ]
-    then 
-      echo "git installation is not success ... check it"
-      exit 1
-  else
-    echo "git installation is success"
-  fi
+      echo "git is not installed , going to install it"
+      dnf install git -y
+      if [ $? -ne 0 ]
+      then 
+          echo "git installation is not success ... check it"
+          exit 1
+      else
+          echo "git installation is success"
+      fi
  
- else
-   echo "git is already installed, nothing to do"
+else
+    echo "git is already installed, nothing to do"
 
 fi
